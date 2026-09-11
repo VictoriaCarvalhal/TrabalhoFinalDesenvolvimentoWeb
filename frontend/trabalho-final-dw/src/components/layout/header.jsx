ee1914f97344842826312f48bc92fd225081f7b9
Pedro Henrique <phenrique0487@gmail.com>
2026-09-11 20:08:31 -0300

    adicionando atributos ARIA em elementos do header e do footer da página

--- frontend/trabalho-final-dw/src/components/layout/header.jsx
+++ frontend/trabalho-final-dw/src/components/layout/header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <div className="d-flex flex-column flex-md-row align-items-center justify-content-between px-3 gap-3">
                 
                 <div className="d-flex justify-content-center justify-content-md-start w-100 header-col-side">
-                    <a href='https://www.uerj.br' target="_blank" rel="noopener noreferrer"><img src={pr3Logo} alt="pr3 logo" className="img-fluid header-logo" /></a>
+                    <a href='https://www.uerj.br' target="_blank" rel="noopener noreferrer"><img src={pr3Logo} aria-label="Logo da Universidade do Estado do Rio de Janeiro, com uma tocha acesa e o número 75 em comemoração aos 75 anos da instituição e ao seu lado o logo da Pró Reitoria de Extensão e Cultura" alt="" title="Logos da UERJ e da PR3" className="img-fluid header-logo" /></a>
                 </div>
 
                 <div className="text-center w-100 header-col-center">
