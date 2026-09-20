5431aa2227c75dffbfd129dbd23011fe78408aab
Eduardo <duedugaranito@gmail.com>
2026-09-19 23:54:29 -0300

    adiciona dependencias e o swagger

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,4 +12,7 @@ urlpatterns = [
     path("api/v1/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/v1/", include("core.urls")),
     path("api/v1/", include("projetos.urls")),
+    # Swagger
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
